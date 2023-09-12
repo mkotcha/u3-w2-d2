a3e8d843b82730f2603d4601705945a8953197a5
Matteo Falasca <matteo.falasca@emmek.org>
2023-09-12 13:49:16 +0200

    addcomment hoocked

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,38 +1,52 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import { Alert, Button, Form, Modal } from "react-bootstrap";
 
-class AddComment extends Component {
-  state = {
-    hasAlert: false,
-    alert: { message: "", status: "", variant: "" },
-    modalShow: false,
-    comment: {
-      comment: "",
-      elementId: "",
-      rate: 1,
-    },
+const AddComment = props => {
+  // state = {
+  //   hasAlert: false,
+  //   alert: { message: "", status: "", variant: "" },
+  //   modalShow: false,
+  //   comment: {
+  //     comment: "",
+  //     elementId: "",
+  //     rate: 1,
+  //   },
+  // };
+
+  const [hasAlert, setHasAlert] = useState(false);
+  const [alert, setAlert] = useState({ message: "", status: "", variant: "" });
+  const [modalShow, setModalShow] = useState(false);
+  const [commentObj, setCommentObj] = useState({
+    comment: "",
+    elementId: "",
+    rate: 1,
+  });
+
+  useEffect(props.update, [modalShow]);
+
+  useEffect(() => setCommentObj({ ...commentObj, elementId: props.selected }), [props.selected]);
+  useEffect(() => setCommentObj({ ...commentObj, elementId: props.selected }), []);
+
+  const handleClose = () => {
+    setModalShow(false);
   };
 
-  handleClose = () => {
-    this.setState({ modalShow: false });
+  const handleShow = () => {
+    setModalShow(true);
   };
 
-  handleShow = () => {
-    this.setState({ modalShow: true });
+  const handleChange = (propertyName, propertyValue) => {
+    setCommentObj({ ...commentObj, [propertyName]: propertyValue });
   };
 
-  handleChange = (propertyName, propertyValue) => {
-    this.setState({ comment: { ...this.state.comment, [propertyName]: propertyValue } });
-  };
-
-  handleSubmit = async event => {
+  const handleSubmit = async event => {
     event.preventDefault();
     const url = "https://striveschool-api.herokuapp.com/api/comments/";
     let options = {};
 
     options = {
       method: "POST",
-      body: JSON.stringify(this.state.comment),
+      body: JSON.stringify(commentObj),
       headers: {
         "Content-Type": "application/json",
         Authorization:
@@ -43,101 +57,96 @@ class AddComment extends Component {
     try {
       const response = await fetch(url, options);
       if (response.ok) {
-        this.setState({
-          comment: {
-            comment: "",
-            elementId: this.props.selected,
-            rate: 1,
-          },
+        setCommentObj({
+          comment: "",
+          elementId: props.selected,
+          rate: 1,
         });
 
         const newComment = await response.json();
         // this.props.mod(newComment._id);
-        this.props.update();
+        // this.props.update();
 
-        this.setState({
-          hasAlert: true,
-          alert: {
-            message: "comment - " + newComment.comment + " - added!",
-            status: response.status,
-            variant: "success",
-          },
+        setHasAlert(true);
+        setAlert({
+          message: "comment - " + newComment.comment + " - added!",
+          status: response.status,
+          variant: "success",
         });
-        setTimeout(() => this.setState({ hasAlert: false }), 2500);
+
+        setTimeout(() => setHasAlert(false), 2000);
       } else {
-        this.setState({
-          hasAlert: true,
-          alert: { message: "Error data", status: response.status, variant: "danger" },
-        });
-        setTimeout(() => this.setState({ hasAlert: false }), 2000);
+        setHasAlert(true);
+        setAlert({ message: "Error data", status: response.status, variant: "danger" });
+
+        setTimeout(() => setHasAlert(false), 2000);
       }
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.log(error);
     }
 
-    setTimeout(() => this.handleClose(), 2000);
+    setTimeout(() => handleClose(), 2000);
   };
 
-  componentDidUpdate = pervProps => {
-    if (pervProps.selected !== this.props.selected) {
-      this.setState({ comment: { ...this.state.comment, elementId: this.props.selected } });
-    }
-  };
+  // componentDidUpdate = pervProps => {
+  //   if (pervProps.selected !== this.props.selected) {
+  //     this.setState({ comment: { ...this.state.comment, elementId: this.props.selected } });
+  //   }
+  // };
 
-  componentDidMount = () => {
-    this.setState({ comment: { ...this.state.comment, elementId: this.props.selected } });
-  };
+  // componentDidMount = () => {
+  //   this.setState({ comment: { ...this.state.comment, elementId: this.props.selected } });
+  // };
 
-  render() {
-    return (
-      <>
-        <Button variant="primary" className="w-100 mt-2" onClick={this.handleShow}>
-          add comment
-        </Button>
+  return (
+    <>
+      <Button variant="primary" className="w-100 mt-2" onClick={handleShow}>
+        add comment
+      </Button>
 
-        <Modal show={this.state.modalShow} onHide={this.handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title>add comment</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            {this.state.hasAlert && (
-              <Alert variant={this.state.alert.variant}>
-                {this.state.alert.message}, status code: {this.state.alert.status}
-              </Alert>
-            )}
-            <Form id="addComment">
-              <Form.Group>
-                <Form.Label>Rating</Form.Label>
-                <Form.Range
-                  min="1"
-                  max="5"
-                  step="1"
-                  value={this.state.comment.rate}
-                  onChange={event => this.handleChange("rate", event.target.value)}
-                />
-                <Form.Label>Comment</Form.Label>
-                <Form.Control
-                  type="text"
-                  as="textarea"
-                  placeholder="your comment..."
-                  value={this.state.comment.comment}
-                  onChange={event => this.handleChange("comment", event.target.value)}
-                  required
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
-            </Button>
-            <Button type="submit" form="addComment" variant="primary" onClick={this.handleSubmit}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </>
-    );
-  }
-}
+      <Modal show={modalShow} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>add comment</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {hasAlert && (
+            <Alert variant={alert.variant}>
+              {alert.message}, status code: {alert.status}
+            </Alert>
+          )}
+          <Form id="addComment">
+            <Form.Group>
+              <Form.Label>Rating</Form.Label>
+              <Form.Range
+                min="1"
+                max="5"
+                step="1"
+                value={commentObj.rate}
+                onChange={event => handleChange("rate", event.target.value)}
+              />
+              <Form.Label>Comment</Form.Label>
+              <Form.Control
+                type="text"
+                as="textarea"
+                placeholder="your comment..."
+                value={commentObj.comment}
+                onChange={event => handleChange("comment", event.target.value)}
+                required
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button type="submit" form="addComment" variant="primary" onClick={handleSubmit}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
 export default AddComment;
